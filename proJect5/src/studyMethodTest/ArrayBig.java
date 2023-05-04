56cf85b5e6eb1c0d70669923fe6fe590e7812618
leekiyeong <132434439+leekiyeong@users.noreply.github.com>
2023-05-04 16:49:22 +0900

    Update ArrayBig.java

--- proJect5/src/studyMethodTest/ArrayBig.java
+++ proJect5/src/studyMethodTest/ArrayBig.java
@@ -6,12 +6,10 @@ public class ArrayBig {
 		int[] arr = {120,30,40,50};
 		System.out.println(ArrayBig(arr));
 	}
-	public static int ArrayBig(int arr[]) {
-		int max = arr[0];
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]>max) {
-				max = arr[i];
-			}
+	public static int ArrayBig(int[] n1) {
+		int max = n1[0];
+		for(int n : n1) {
+			max = Math.max(max,n);
 		 }
 		return max;
 	}
