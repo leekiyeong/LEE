f81e14369b1e748652bd67edc7f8a8ee93110ce6
leekiyeong <132434439+leekiyeong@users.noreply.github.com>
2023-05-04 16:49:52 +0900

    Update ArraySmall.java

--- proJect5/src/studyMethodTest/ArraySmall.java
+++ proJect5/src/studyMethodTest/ArraySmall.java
@@ -1,19 +1,17 @@
 package studyMethodTest;
 
 public class ArraySmall {
-
+	
 	public static void main(String[] args) {
-		int[] arr = {120,30,20,50};
+		int[] arr = {120,30,40,50};
 		System.out.println(ArraySmall(arr));
-
 	}
-	public static int ArraySmall(int arr[]) {
-		int min = arr[0];
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]<min) {
-				min = arr[i];
-			}
+	public static int ArraySmall(int[] n1) {
+		int min = n1[0];
+		for(int n : n1) {
+			min = Math.min(min,n);
 		 }
 		return min;
 	}
 }
+
